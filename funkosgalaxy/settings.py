a6051694e28fab068f35fbebab84a0fcb7f67929
ocimar84 <ocimarcostaf@gmail.com>
2023-10-30 12:29:54 +0500

    Fix Mail sending

--- funkosgalaxy/settings.py
+++ funkosgalaxy/settings.py
@@ -47,7 +47,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if IS_HEROKU:
-    DEBUG = False
+    DEBUG = True
 else:
     DEBUG = True
 
