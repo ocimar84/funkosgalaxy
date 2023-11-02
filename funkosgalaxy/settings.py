0808887375224dfcca01fbec802dba0f54bba551
ocimar84 <ocimarcostaf@gmail.com>
2023-11-03 00:47:19 +0500

    Fixes

--- funkosgalaxy/settings.py
+++ funkosgalaxy/settings.py
@@ -46,7 +46,7 @@ EMAIL_TIMEOUT = 10
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
