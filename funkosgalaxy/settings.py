2b03970bdc23b639fdd950b3b3ff310cce6479a2
ocimar84 <ocimarcostaf@gmail.com>
2023-11-04 14:47:46 +0500

    Email confirmation

--- funkosgalaxy/settings.py
+++ funkosgalaxy/settings.py
@@ -53,7 +53,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if IS_HEROKU:
-    DEBUG = False
+    DEBUG = True
 else:
     DEBUG = True
 
